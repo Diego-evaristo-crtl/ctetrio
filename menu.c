fc7a9618f2741f9c1a22988199665824bfb18396
Diego Evaristo <di.diegoevaristo@gmail.com>
2025-10-06 20:26:23 -0300

    Finishing pause-menu "API"

--- menu.c
+++ menu.c
@@ -4,12 +4,22 @@
 
 
 static WINDOW *menu_win;
+static size_t moptind; 	/* Current used size of mopts */
+static size_t moptlen; 	/* Current allocated size of mopts */
+static size_t copt;	/* Current selected option, index in mopts*/
+struct menuopt **mopts; /* Options of the menu */
+
+/* Redraws the menu, is the option currently choosen */
+static void updmenu() __attribute__((nothrow));
 
 
 int mkmenu(unsigned int xlen, unsigned int ylen)
 {
 	int x;
 	int y;
+
+	moptind = 0;
+	moptlen = 0;
 
 	getmaxyx(stdscr, y, x);
 	clear();
@@ -25,8 +35,76 @@ int mkmenu(unsigned int xlen, unsigned int ylen)
 
 void demenu(void)
 {
+	if (mopts)
+		free(mopts);
 	wclear(menu_win);
 	wrefresh(menu_win);
 	delwin(menu_win);
 	menu_win = NULL;
+}
+
+int addmopt(struct menuopt *opt)
+{
+	if (!mopts && !(mopts = malloc((moptlen = 1) * sizeof(*mopts))))
+		return -1;
+	else if (moptind >= moptlen && !(mopts = realloc(mopts, (moptlen <<= 1)
+	* sizeof(*mopts))))
+		return -1;
+	mopts[moptind++] = opt;
+	return 0;
+}
+
+void *menuwait(void)
+{
+	int ch;
+
+	updmenu(copt);
+	while (1) {
+		switch ((ch = getch())) {
+		case KEY_UP:
+			if (copt > 0) {
+				--copt;
+				updmenu(copt);
+			}
+			break;
+		case KEY_DOWN:
+			if (copt + 1 < moptind) {
+				++copt;
+				updmenu(copt);
+			}
+			break;
+		case '\n':
+		case '\r':
+		case KEY_RIGHT:
+			return mopts[copt]->optf(mopts[copt]->arg);
+		}
+	}
+
+	return NULL;
+}
+
+static void updmenu()
+{
+	size_t i;
+	short save_fg;
+	short save_bg;
+
+	pair_content(MENU_COLOR, &save_fg, &save_bg);
+	init_pair(MENU_COLOR, COLOR_BLACK, COLOR_WHITE);
+
+	wclear(menu_win);
+	box(menu_win, 0, 0);
+
+	for (i = 0; i < moptind; ++i) {
+		if (i == copt) {
+			wattron(menu_win, COLOR_PAIR(MENU_COLOR));
+			mvwprintw(menu_win, (int)i + 1, 1, mopts[i]->opts);
+			wattroff(menu_win, COLOR_PAIR(MENU_COLOR));
+		} else {
+			mvwprintw(menu_win, (int)i + 1, 1, mopts[i]->opts);
+		}
+	}
+
+	wrefresh(menu_win);
+	init_pair(MENU_COLOR, save_fg, save_bg);
 }
--- menu.h
+++ menu.h
@@ -1,9 +1,22 @@
 #ifndef CTETRIO_MENU_H
 #define CTETRIO_MENU_H	1
 
-/* Creates a menu, returns -1 if one alredy exists */
+#include <curses.h>
+#define MENU_COLOR 1
+
+struct menuopt {
+	const char *opts;
+	void *arg;
+	void *(*optf)(void *);
+};
+
+/* Creates a menu, returns -1 if it fails */
 int mkmenu(unsigned int xlen, unsigned int ylen) __attribute__((nothrow, cold));
 /* Deletes a menu */
 void demenu(void) __attribute__((nothrow, cold));
+/* Add and options to the menu, returns -1 if it fails */
+int addmopt(struct menuopt *opt) __attribute__((nothrow, nonnull, cold));
+/* Waits for an action (calling an .optf) on the menu */
+void *menuwait(void) __attribute__((nothrow));
 
 #endif /* CTETRIO_MENU_H */
